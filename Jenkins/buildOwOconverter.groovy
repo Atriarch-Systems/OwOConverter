f56e1e0053cfc791d5f21315b5b645f71f0e2571
Demonslyr <Demonslyr@users.noreply.github.com>
2020-04-04 18:52:21 -0500

    Update buildOwOconverter.groovy

--- Jenkins/buildOwOconverter.groovy
+++ Jenkins/buildOwOconverter.groovy
@@ -25,7 +25,7 @@ node {
         }
     }
     stage('deploy'){        
-        def deployout = sh(returnStdout: true, script: "kubectl apply -f ${k8sDeployYamlPath} -n Fun")
+        def deployout = sh(returnStdout: true, script: "kubectl apply -f ${k8sDeployYamlPath} -n owoconverter")
         //def updateout = sh(returnStdout: true, script: '''kubectl patch deployment fah-cpu -n Fun -p "{\\"spec\\":{\\"template\\":{\\"metadata\\":{\\"labels\\":{\\"date\\":\\"`date +'%s'`\\"}}}}}"''')
         println deployout        
     }                
