455969ea252d4a72ffb67c2e1a1c815be6a9e46d
Demonslyr <Demonslyr@users.noreply.github.com>
2020-04-04 20:29:19 -0500

    Update buildOwOconverter.groovy

--- Jenkins/buildOwOconverter.groovy
+++ Jenkins/buildOwOconverter.groovy
@@ -9,23 +9,24 @@ node {
         dockerfilePathFromRoot = "./Dockerfile"// this is the path from the base directory
         k8sDeployYamlPath = "./service.yaml"
         k8sDeployName = ""
+        imageVersion = "v1.0.${env.BUILD_NUMBER}"
     }
     stage('build'){
        def buildout = sh(returnStdout: true, script: "docker build -t ${appName} -f ${dockerfilePathFromRoot} .")
        println buildout
     }
     stage('push'){
-        def tagout = sh(returnStdout: true, script: "docker tag ${appName} ${dockerRepo}/${appName}:latest")
+        def tagout = sh(returnStdout: true, script: "docker tag ${appName} ${dockerRepo}/${appName}:${imageVersion}")
         println tagout
         withCredentials([usernamePassword(usernameVariable: "DOCKER_USER",passwordVariable: "DOCKER_PASS", credentialsId: dockerCredId)]){
             def loginout = sh(returnStdout: true, script: "echo ${DOCKER_PASS} | docker login --username ${DOCKER_USER} --password-stdin")
             println loginout
-            def pushout = sh(returnStdout: true, script: "docker push ${dockerRepo}/${appName}:latest")
+            def pushout = sh(returnStdout: true, script: "docker push ${dockerRepo}/${appName}:${imageVersion}")
             println pushout
         }
     }
     stage('deploy'){        
-        def deployout = sh(returnStdout: true, script: "kubectl apply -f ${k8sDeployYamlPath}")
+        def deployout = sh(returnStdout: true, script: " export IMAGE_VERSION=${imageVersion} && envsubst < ${k8sDeployYamlPath} | kubectl apply -f -")
         //def updateout = sh(returnStdout: true, script: '''kubectl patch deployment fah-cpu -n Fun -p "{\\"spec\\":{\\"template\\":{\\"metadata\\":{\\"labels\\":{\\"date\\":\\"`date +'%s'`\\"}}}}}"''')
         println deployout        
     }                
