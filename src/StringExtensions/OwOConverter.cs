894da561a23dd529ceccc394d457cf441e95be1c
Daniel Murtha <Dr.Murtha@me.com>
2020-08-16 04:37:16 -0500

    Spruced it up. Turns out I needed an IDE uwu

--- src/StringExtensions/OwOConverter.cs
+++ src/StringExtensions/OwOConverter.cs
@@ -9,7 +9,7 @@ namespace OwOConverter.StringExtensions.OwOConverter
         private static readonly Random RandomInt = new Random((int)DateTime.UtcNow.Ticks);
         private static readonly List<string> Faces = new List<string> { ";w;", "owo", "UwU", ">w<", "^w^", "*w*" };//"(`・ω・´)(・`ω´・)" doesn't encode right :C
         private static readonly MatchEvaluator FaceEvaluator = _ => $" {Faces[RandomInt.Next(Faces.Count)]} ";
-        private static readonly IDictionary<string, string> map = new Dictionary<string, string>{
+        private static readonly IDictionary<string, string> Map = new Dictionary<string, string>{
             {@"(r|l)", "w"},
             {@"(R|L)", "W"},
             {"n([aeiou])", @"ny$1"},
@@ -18,10 +18,10 @@ namespace OwOConverter.StringExtensions.OwOConverter
             {@"(ove)", "uv"},
             {@"(ou)", "ew"}
         };
-        private static readonly Regex regex = new Regex(String.Join("|",map.Keys));
+        private static string pattern = string.Join("|", Map.Keys);
         public static string ConvertToOwO(this string input)
         {
-            regex.Replace(input, m => map[m.Value], RegexOptions.Multiline,TimeSpan.FromSeconds(1));
+            Regex.Replace(input, pattern, m => Map[m.Value], RegexOptions.Multiline,TimeSpan.FromSeconds(1));
             input = Regex.Replace(input, @"\!+", FaceEvaluator);
             return input;
         }
