a9c76985126f01990ffb55d79b8d806ba2beafd1
Daniel Murtha <Dr.Murtha@me.com>
2020-08-22 02:19:44 -0500

    fixed namespace masnaming

--- src/Startup.cs
+++ src/Startup.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using OwOConverter.StringExtensions.OwOConverter;
+using OwOConverter.StringExtensions;
 
 namespace OwOConverter
 {
--- src/StringExtensions/OwOConverter.cs
+++ src/StringExtensions/OwOConverter.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text.RegularExpressions;
 
-namespace OwOConverter.StringExtensions.OwOConverter
+namespace OwOConverter.StringExtensions
 {
     public static class OwOConverter
     {
